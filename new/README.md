d132b7a4d7d3290a9b1f704f08bf7a1cbfa76d48
IanoTech <ian-yitzhak>
2024-02-11 20:40:06 +0300

    initial commit

---
+++ new/README.md
@@ -0,0 +1,10 @@
+##Django CRUD Application Documentation
+
+#Introduction
+This documentation provides an overview of the CRUD (Create, Read, Update, Delete) application built using Django framework. The application allows users to manage a list of books by performing CRUD operations.
+
+#Features
+Add a new book with title and author details.
+View a list of existing books.
+Delete a book from the list.
+Confirmation popup before deleting a book.
---
+++ new/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .models import Book
+from .forms import BookForm
+
+#get all books
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/book_list.html', {'books': books})
+
+#add a book
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/add_book.html', {'form': form})
+
+#delete a book
+def delete_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    book.delete()
+    return redirect('book_list')
+
+
